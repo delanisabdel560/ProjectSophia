274762c0b7c8dcf71f8eb4ba1f555ddf7d1d8f3d
delanisabdel560 <delanisabdel560@gmail.com>
2025-03-24 09:40:45 +0000

    Add 18.cpp

---
+++ src/18.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+
+    if (num1 > num2) {
+        cout << "The first number is larger." << endl;
+    } else {
+        cout << "The second number is larger." << endl;
+    }
+
+    return 0;
+}
