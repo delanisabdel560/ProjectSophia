e0240844133f92febce6e2d48c0dfb3e41841889
delanisabdel560 <delanisabdel560@gmail.com>
2025-04-25 08:29:45 +0000

    Add 32.cpp

---
+++ src/32.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+    if (n > 0) {
+        cout << "N is greater than zero." << endl;
+    } else {
+        cout << "N is not greater than zero." << endl;
+    }
+    return 0;
+}
